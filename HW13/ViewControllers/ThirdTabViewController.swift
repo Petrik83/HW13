3d610475edda281e2732aeb9f3cb18a70fcc79f1
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2021-12-27 07:51:39 +0300

    Created view files.

---
+++ HW13/ViewControllers/ThirdTabViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdTabViewController.swift
+//  HW13
+//
+//  Created by Александр Петрович on 27.12.2021.
+//
+
+import UIKit
+
+class ThirdTabViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGray6
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
