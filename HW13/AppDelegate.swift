3942546908ecd41534e60d661e20b4ad7dbac80b
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2021-12-27 07:57:58 +0300

    Added TabBarController and configured it.

--- HW13/AppDelegate.swift
+++ HW13/AppDelegate.swift
@@ -9,15 +9,35 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        let tabBarController = UITabBarController()
         
+        let firstTabViewController = FirstTabViewController()
+        firstTabViewController.tabBarItem = UITabBarItem(title: "Медиатека", image: UIImage(named: "photo"), tag: 0)
+        
+        let secondTabViewController = SecondTabViewController()
+        secondTabViewController.tabBarItem = UITabBarItem(title: "Для Вас", image: UIImage(named: "custom.heart"), tag: 1)
+
+        let thirdTabViewController = ThirdTabViewController()
+        thirdTabViewController.tabBarItem = UITabBarItem(title: "Альбомы", image: UIImage(named: "custom.square"), tag: 2)
+        
+        let fourthTabViewController = FourthTabViewController()
+        fourthTabViewController.tabBarItem = UITabBarItem(title: "Поиск", image: UIImage(named: "custom.magnifyin"), tag: 3)
+        tabBarController.setViewControllers([firstTabViewController,
+                                             secondTabViewController,
+                                             thirdTabViewController,
+                                             fourthTabViewController], animated: true)
+        tabBarController.selectedIndex = 2
+        
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
         
         return true
     }
-
+    
 }
 
